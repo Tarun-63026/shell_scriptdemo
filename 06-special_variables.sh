8371275336e2354204b843c7096eb60efc8020e6
Tarun-63026 <lukalapunarru680@gmail.com>
2026-09-12 20:39:03 +0530

    variables

---
+++ 06-special_variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "all variables: $@"
+echo "No of variables: $#"
+echo "Script name : $0"
+echo "Present working directory : $PWD"
+echo "Which user running the script: $USER"
+echo "HOSTNAME: $HOSTNAME"
+echo "PID of the current process: $$"
+sleep 60
+echo "PID of the last background process: $!"
