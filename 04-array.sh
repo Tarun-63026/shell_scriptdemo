57f0e4d5f260ca705243596fd3e7c8f967658f16
Tarun-63026 <lukalapunarru680@gmail.com>
2026-09-12 20:19:24 +0530

    variables

---
+++ 04-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOVIES=(
+    "The Shawshank Redemption"
+    "The Godfather"
+    "The Dark Knight"
+    "Pulp Fiction"
+    "The Lord of the Rings: The Return of the King"
+)
+
+echo "First moive name is: {$MOVIES [0] }"
