65032a88ac4e58bd9660ee39734c87478735afed
Tarun-63026 <lukalapunarru680@gmail.com>
2026-09-12 20:54:21 +0530

    variables

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-NUMBER=$!
+NUMBER=$1
 
-if [ $NUMBER -gt 10 ]
-then
+if [ $NUMBER -gt 10 ]; then
     echo "Given $NUMBER is greater than 10"
 else
     echo "Given $NUMBER is less than 10"
