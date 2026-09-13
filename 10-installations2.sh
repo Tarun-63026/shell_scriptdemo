b758f285ab9802c8e268d030e752b0df327d4643
Tarun-63026 <lukalapunarru680@gmail.com>
2026-09-13 22:25:58 +0530

    Loops

--- 10-installations2.sh
+++ 10-installations2.sh
@@ -14,9 +14,9 @@ VALIDATE(){
     if [ $1 -ne 0 ]; then
       echo -e "$2...$R Faliure $N"
     else
-      echo -e "$2...$G Success $N
+      echo -e "$2...$G Success $N"
     fi
-}
+ }
 
 if [ $USERID -ne 0 ]; then
    echo "Please swicth to the super user"
@@ -33,6 +33,6 @@ do
     echo -e "$i is already installed...$Y Skipping $N"
    else
      dnf install $i -y &>>LOGFILE
-     VALIDATE $? "Installtion of $i"
+     VALIDATE $? Installtion of $i
    fi
 done
