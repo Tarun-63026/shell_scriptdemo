1e4184c82389d63b346c914dabf235f92e662687
Tarun-63026 <lukalapunarru680@gmail.com>
2026-09-13 20:15:34 +0530

    Loops

---
+++ 10-installations2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%s)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=\tmp\$SCRIPTNAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]l; then
+      echo "$2...Failure"
+    else
+      echo "$2...Success"
+    fi
+}
+
+if [ $USERID -ne 0 ]; then
+   echo "Please swicth to the super user"
+else
+   echo "You are the super user"
+fi
+
+dnf install mysql -y &>>LOGFILE
+VALIDATE $? echo "Installtion of Mysql"
