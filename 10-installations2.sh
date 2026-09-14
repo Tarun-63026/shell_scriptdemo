ca904efb7d01611d5255ea941c611135c11e68d6
Tarun-63026 <lukalapunarru680@gmail.com>
2026-09-14 22:17:59 +0530

    Installation

--- 10-installations2.sh
+++ 10-installations2.sh
@@ -5,12 +5,17 @@ TIME_STAMP=$(date +F%-H%-M%-S%)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 VALIDATE(){
    if [ $1 -ne 0 ]; then
-     echo "$2...Failure"
+     echo -e "$2... $R Failure $N"
      exit
    else
-     echo "$2...Success"
+     echo -e "$2... $G Success $N" 
    fi
 }
 
@@ -27,7 +32,7 @@ do
   echo "Package to Install: $i"
   dnf list installed $i &>>LOGFILE
   if [ $? -eq 0 ]; then
-    echo "$i was already installed"
+    echo -e "$i was already installed... $Y Skipping $N"
   else
      dnf install $i -y &>>LOGFILE
      VALIDATE $? "Instlattion of $i"
